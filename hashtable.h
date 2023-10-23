d463ae3f0fd5ba6b6baddc6ce7d7f4280ca62799
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-10-23 11:27:22 -0500

    Created hashtable class

--- hashtable.h
+++ hashtable.h
@@ -1,3 +1,28 @@
 /* 
  * your comment header here
  */
+ 
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include "linkedlist.h"
+#include <string>
+
+#define HASHTABLESIZE 15
+
+class HashTable {
+public:
+    HashTable();
+    ~HashTable();
+
+    bool insertEntry(int id, std::string* data);
+    std::string getData(int id);
+    bool removeEntry(int id);
+    int getCount();
+    void printTable();
+
+private:
+
+};
+
+#endif /* HASHTABLE_H */
