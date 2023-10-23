9682ff528705e1acb1ab40e4d9f265b531167761
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-10-23 15:26:51 -0500

    Added insertEntry

--- hashtable.cpp
+++ hashtable.cpp
@@ -5,11 +5,10 @@
 #include "hashtable.h"
 
 HashTable::HashTable() {
-    // Initialize the hash table with linked lists
+    
 }
 
 HashTable::~HashTable() {
-    // Destructor to clean up resources
     for (int i = 0; i < HASHTABLESIZE; i++) {
         table[i].clearList();
     }
@@ -17,12 +16,12 @@ HashTable::~HashTable() {
 
 bool HashTable::insertEntry(int id, std::string* data) {
     if (id <= 0 || data == nullptr || data->empty()) {
-        return false; // Reject bad IDs or strings
+        return false; 
     }
 
     int index = hash(id);
     if (table[index].exists(id)) {
-        return false; // ID must be unique
+        return false; 
     }
 
     Data entry;
@@ -32,47 +31,3 @@ bool HashTable::insertEntry(int id, std::string* data) {
 
     return true;
 }
-
-string HashTable::getData(int id) {
-    int index = hash(id);
-    Data entry;
-
-    if (table[index].getNode(id, &entry)) {
-        return entry.data;
-    } else {
-        return ""; // Return an empty string if the id does not exist
-    }
-}
-
-bool HashTable::removeEntry(int id) {
-    int index = hash(id);
-    if (table[index].deleteNode(id)) {
-        return true;
-    }
-    return false; // Entry not found
-}
-
-int HashTable::getCount() {
-    int count = 0;
-    for (int i = 0; i < HASHTABLESIZE; i++) {
-        count += table[i].getCount();
-    }
-    return count;
-}
-
-void HashTable::printTable() {
-    for (int i = 0; i < HASHTABLESIZE; i++) {
-        std::cout << "Entry " << i + 1 << ": ";
-        if (table[i].getCount() == 0) {
-            std::cout << "EMPTY";
-        } else {
-            table[i].printList();
-        }
-        std::cout << std::endl;
-    }
-}
-
-int HashTable::hash(int id) {
-    return id % HASHTABLESIZE;
-}
-
